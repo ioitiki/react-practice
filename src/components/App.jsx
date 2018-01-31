ac11828c23f74e20eab95e92cd946f550eb47570
Andrew Malkin <andymalkin@hotmail.com>
2018-01-31 11:47:44 -0800

    add an About component and add the link to the header.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Error404 from './Error404';
 import Home from './Home';
+import About from './About';
 import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
         <Route component={Error404} />
       </Switch>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div>
       <h1>HEY YOOOO!</h1>
-      <Link to="/">Home</Link>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link>
     </div>
   );
 }
